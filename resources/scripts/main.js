0d093d11ab475a2055775d787dabf2c20e825261
Louis Orleans <louis@0rleans.com>
2015-02-28 03:44:31 -0800

    um. a bunch of new stuff. I should try to keep track of what I change
    Color change for logo
    new breaker images
    forms for sponsors and volunteers
    better looking FAB
    FAB auto changes based on page scroll

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -9,26 +9,17 @@ function setSlowScroll (that) {
 
 
 document.onscroll = onscroll;
+var breakers = [];
 function onscroll () {
-//	var ratio = window.getComputedStyle(document.getElementById('top-trapezoid')).getPropertyValue('height').split("px")[0] / window.getComputedStyle(document.getElementById('headerImg')).getPropertyValue('height').split("px")[0];
-//
-//	var _scrollY = window.scrollY * -ratio * 2 / 3 + 30;
-//	_scrollY = Math.min(Math.max(_scrollY, -66), 30);
-//	document.getElementById('top-trapezoid').style.transform = "translateY(" + (_scrollY - 30) + "px)";
-//	document.getElementById('top-trapezoid').style.webkitTransform = "translateY(" + (_scrollY - 30) + "px)";
-//
-//	document.getElementById('linksBefore').style.left = (-_scrollY) + "px";
-//	document.getElementById('linksAfter').style.right = (-_scrollY) + "px";
-//
-//	document.getElementById('siteIcon').style.width = (0.88421 * _scrollY + 103.47368) + "px";
-//	document.getElementById('siteIcon').style.left = (-0.52631579 * _scrollY + 75.789474 + 270) + "px";
-//	document.getElementById('siteIcon').style.transform = "translateY(" + (0.05263158 * _scrollY) + "px)";
-//	document.getElementById('siteIcon').style.webkitTransform = "translateY(" + (0.05263158 * _scrollY) + "px)";
-//	document.getElementById('headerImg').style.transform = "translateY(" + ((window.scrollY * -ratio * 2 + 30) / 5 - 7) + "px)";
-//    document.getElementById('headerImg').style.webkitTransform = "translateY(" + ((window.scrollY * -ratio * 2 + 30) / 5 - 7) + "px)";
-    
     for (var i = 0; i < specialScrolls.length; i++) {
         specialScrolls[i].onscroll(window.scrollY);
+    }
+    for (var i = 0; i < breakers.length; i++) {
+        if (breakers[i].getBoundingClientRect().top < window.innerHeight) {
+            if (breakers[i].topVisible == null)
+                breakers[i].topVisible = window.scrollY;
+            breakers[i].style.backgroundPositionY = (((window.scrollY - breakers[i].topVisible) / (window.innerHeight + breakers[i].getBoundingClientRect().height)) * 100) + "%";
+        }
     }
 }
 
@@ -37,10 +28,18 @@ function onload () {
     
     setFastScroll(document.getElementById("title"));
     setSlowScroll(document.getElementById("headerImg"));
+    breakers = [document.getElementById("breaker1"), document.getElementById("breaker2")];
     
     var fixedFAB = document.getElementById("downArrow");
     var pos = fixedFAB.getBoundingClientRect();
     originalPos = {right:window.innerWidth - (pos.left + pos.width), bottom:window.innerHeight - (pos.top + pos.height)};
+    specialScrolls.push({applyTo:fixedFAB, onscroll:function (scroll) {
+        if (this.applyTo.getBoundingClientRect().top < 0) {
+            switchToFAB();
+        } else if (scroll === 0 && this.applyTo.style.display === "none") {
+            switchToFAB();
+        }
+    }});
 }
 
 function scrollToTopOfContent () {
@@ -66,26 +65,24 @@ function switchToFAB () {
     
     if (FAB.style.display === "none" || FAB.style.display === "") {
         var pos = fixedFAB.getBoundingClientRect();
-        FAB.style.right = window.innerWidth - (pos.left + pos.width) + "px";
-        FAB.style.bottom = window.innerHeight - (pos.top + pos.height) + "px";
-        FAB.style.transform = "scale(1.3)";
+        FAB.style.right = window.innerWidth - (pos.left + pos.width) - 15 + "px";
+        FAB.style.bottom = window.innerHeight - (pos.top + pos.height) - 7 + "px";
+        FAB.style.transform = "scale(1.3) rotateZ(180deg)";
         fixedFAB.style.display = "none";
         FAB.style.display = "block";
 
-        TweenLite.to(FAB, tweenTime, {
+        TweenLite.to(FAB, 0.5, {
             bottom: "25px",
-            right: "25px",
-            transform: "scale(1.3) rotateZ(180deg)"
+            right: "25px"
         });
     } else {
         FAB.style.bottom = "25px";
         FAB.style.right = "25px";
-        FAB.style.transform = "scale(1.3)";
+        FAB.style.transform = "scale(1.3) rotateZ(0deg)";
 
-        TweenLite.to(FAB, tweenTime + 0.2, {
-            right: originalPos.right + "px",
-            bottom: originalPos.bottom + "px",
-            transform: "scale(1.3) rotateZ(0deg)",
+        TweenLite.to(FAB, 0.5, {
+            right: originalPos.right - 15 + "px",
+            bottom: originalPos.bottom - 7 + "px",
             onComplete: function () {
                 FAB.style.display = "none";
                 fixedFAB.style.display = "block";
@@ -94,33 +91,171 @@ function switchToFAB () {
     }
 }
 
-function openRegisterForm () {
-    var registerForm = document.getElementById("register");
+function openForm (name, sender) {
+    var form = document.getElementById(name);
+    form.style.left = sender.getBoundingClientRect().left + sender.getBoundingClientRect().width / 2;
+    form.style.top = sender.getBoundingClientRect().top + sender.getBoundingClientRect().height / 2;
+    form.style.transform = "scale(" + sender.getBoundingClientRect().width / form.getBoundingClientRect().width + ", " + sender.getBoundingClientRect().height / form.getBoundingClientRect().height + ")";
     
-    registerForm.style.pointerEvents = "auto";
-    TweenLite.to(registerForm, tweenTime, {opacity: 1, transform: "scale(1, 1)", top: "-600px"});
+    form.style.pointerEvents = "auto";
+    TweenLite.to(form, tweenTime, {opacity: 1, transform: "scale(1, 1)", left: "50%", top: "50%"});
 }
 
-function closeRegisterForm () {
-    var registerForm = document.getElementById("register");
+function closeForm (name, sender) {
+    var form = document.getElementById(name);
     
-    registerForm.style.pointerEvents = "none";
-    TweenLite.to(registerForm, tweenTime, {opacity: 0, transform: "scale(0.22, 0.1)", top: "-430px"});
+    form.style.pointerEvents = "none";
+    TweenLite.to(form, tweenTime, {opacity: 0, transform: "scale(" + sender.getBoundingClientRect().width / form.getBoundingClientRect().width + ", " + sender.getBoundingClientRect().height / form.getBoundingClientRect().height + ")", left: sender.getBoundingClientRect().left + sender.getBoundingClientRect().width / 2, top: sender.getBoundingClientRect().top + sender.getBoundingClientRect().height / 2});
 }
 
-function GetScreenCordinates(obj) {
-    var p = {};
-    p.x = obj.offsetLeft;
-    p.y = obj.offsetTop;
-    while (obj.offsetParent) {
-        p.x = p.x + obj.offsetParent.offsetLeft;
-        p.y = p.y + obj.offsetParent.offsetTop;
-        if (obj == document.getElementsByTagName("body")[0]) {
-            break;
-        }
-        else {
-            obj = obj.offsetParent;
-        }
+function submitForm (name) {
+    if (checkInputForErrors(name)) {
+        // TODO: AJAX the data to the server
+        alert("We're not currently applications.");
     }
-    return p;
+}
+
+function checkInputForErrors (name) {
+    var hasError = false;
+    
+    switch (name) {
+        case "register":
+            var fName = document.getElementById("fName");
+            var lName = document.getElementById("lName");
+            var email = document.getElementById("email");
+            var github = document.getElementById("github");
+            var school = document.getElementById("school");
+            var grade = document.getElementById("");
+            var age = document.getElementById("age");
+            var gender = document.getElementById("");
+            var skills = document.getElementById("skills");
+
+
+            if (fName.value === "") {
+                fName.setAttribute("error", "");
+                fName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (lName.value === "") {
+                lName.setAttribute("error", "");
+                lName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (email.value === "") {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            } else if (email.value.split("@").length == 0 || email.value.split(".").length == 0) {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Please enter a valid email";
+                hasError = true;
+            }
+            if (github.value === "") {
+                github.setAttribute("error", "");
+                github.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (school.value === "") {
+                school.setAttribute("error", "");
+                school.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (age.value === "") {
+                age.setAttribute("error", "");
+                age.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            } else if (age.value < 13) {
+                age.setAttribute("error", "");
+                age.nextSibling.innerHTML = "You must be in high school to attend";
+                hasError = true;
+            }
+            if (skills.value === "") {
+                skills.setAttribute("error", "");
+                skills.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            break;
+        case "volunteer":
+            var fName = document.getElementById("fName");
+            var lName = document.getElementById("lName");
+            var email = document.getElementById("email");
+            var age = document.getElementById("age");
+            var skills = document.getElementById("skills");
+
+
+            if (fName.value === "") {
+                fName.setAttribute("error", "");
+                fName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (lName.value === "") {
+                lName.setAttribute("error", "");
+                lName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (email.value === "") {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            } else if (email.value.split("@").length == 0 || email.value.split(".").length == 0) {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Please enter a valid email";
+                hasError = true;
+            }
+            if (age.value === "") {
+                age.setAttribute("error", "");
+                age.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            } else if (age.value < 18) {
+                age.setAttribute("error", "");
+                age.nextSibling.innerHTML = "You must be in over 18 to volunteer";
+                hasError = true;
+            }
+            if (skills.value === "") {
+                skills.setAttribute("error", "");
+                skills.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            break;
+        case "sponsor":
+            var cName = document.getElementById("cName");
+            var fName = document.getElementById("fName");
+            var lName = document.getElementById("lName");
+            var email = document.getElementById("email");
+            var message = document.getElementById("message");
+
+
+            if (cName.value === "") {
+                cName.setAttribute("error", "");
+                cName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (fName.value === "") {
+                fName.setAttribute("error", "");
+                fName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (lName.value === "") {
+                lName.setAttribute("error", "");
+                lName.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            if (email.value === "") {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            } else if (email.value.split("@").length == 0 || email.value.split(".").length == 0) {
+                email.setAttribute("error", "");
+                email.nextSibling.innerHTML = "Please enter a valid email";
+                hasError = true;
+            }
+            if (message.value === "") {
+                message.setAttribute("error", "");
+                message.nextSibling.innerHTML = "Can not be blank";
+                hasError = true;
+            }
+            break;
+    }
+
+    return !hasError;
 }
