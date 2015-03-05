203f6468c23c53ff9401bb08d0886fad7380f7f9
Louis Orleans <louis@0rleans.com>
2015-03-05 11:24:11 -0800

    Fixing the header sizing for mobile and scrolling on mobile

--- resources/scripts/headers.js
+++ resources/scripts/headers.js
@@ -2,8 +2,10 @@ function SpecialScroll (applyTo, relativeSpeed) {
 	this.relativeSpeed = relativeSpeed;
     this.applyTo = applyTo;
 	this.onscroll = function (scroll) {
-        var newPos = scroll * -this.relativeSpeed;
-        this.applyTo.style.transform = "translateY(" + (newPos) + "px)";
-        this.applyTo.style.webkitTransform = "translateY(" + (newPos) + "px)";
+	    if (!window.mobilecheck()) {
+	        var newPos = scroll * -this.relativeSpeed;
+	        this.applyTo.style.transform = "translateY(" + (newPos) + "px)";
+	        this.applyTo.style.webkitTransform = "translateY(" + (newPos) + "px)";
+	    }
 	}
 }
